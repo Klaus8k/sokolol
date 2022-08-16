b236dd3c6d456946e448a1f515e6d4c756def611
Oleg S home <tipkor@mail.ru>
2022-08-17 01:08:50 +0300

    add radiobutton 4+4

--- order_cost/forms.py
+++ order_cost/forms.py
@@ -1,15 +1,18 @@
 from django import forms
-from django.forms.widgets import NumberInput
+from django.forms.widgets import NumberInput, RadioSelect
+
+paper_weigh = [(130, 130), (170, 170), (300, 300)]
+dublicate_choise = [(True,'4+4'),(False,'4+0')]
+form_wiget = NumberInput(attrs={'class': 'offset_form'})
 
 
 class OffsetForm(forms.Form):
     width = forms.IntegerField(required=None, min_value=0, max_value=1000,
-                               label='Ширина', widget=NumberInput(attrs={'class': 'offset_form'}))
-    higth = forms.IntegerField(
-        required=None, min_value=0, max_value=1000, label='Высота')
-    weigh = forms.ChoiceField(required=None, label='Плотность', choices=[(130, 130),
-                                                                             (170,
-                                                                              170),
-                                                                             (300, 300)])
-    order = forms.IntegerField(
-        required=None, min_value=0, max_value=100000, label='Тираж')
+                                label='Ширина', widget=form_wiget)
+    higth = forms.IntegerField(required=None, min_value=0, max_value=1000,
+                                label='Высота', widget=form_wiget)
+    weigh = forms.ChoiceField(required=None,
+                                label='Плотность', choices=paper_weigh)
+    order = forms.IntegerField(required=None, min_value=0, max_value=100000,
+                                label='Тираж', widget=form_wiget)
+    dublicate = forms.BooleanField(required=None, widget=RadioSelect(choices=dublicate_choise))
