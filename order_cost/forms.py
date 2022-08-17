a5242611e0380f534d1cb305518cc833bb19810d
Oleg Sokolov <klaus888@yandex.ru>
2022-08-17 13:47:09 +0300

    add solvent form

--- order_cost/forms.py
+++ order_cost/forms.py
@@ -1,18 +1,34 @@
 from django import forms
-from django.forms.widgets import NumberInput, RadioSelect
+from django.forms import widgets
 
 paper_weigh = [(130, 130), (170, 170), (300, 300)]
-dublicate_choise = [(True,'4+4'),(False,'4+0')]
-form_wiget = NumberInput(attrs={'class': 'offset_form'})
+dublicate_choise = [(True, '4+4'), (False, '4+0')]
+form_wiget = widgets.NumberInput(attrs={'class': 'offset_form'})
 
 
 class OffsetForm(forms.Form):
+    type_order = forms.CharField(initial='offset', label='Офсетная печать',
+                                 widget=widgets.TextInput(attrs={'style': 'display: none'}))
     width = forms.IntegerField(required=None, min_value=0, max_value=1000,
-                                label='Ширина (мм)', widget=form_wiget)
+                               label='Ширина (мм)', widget=form_wiget)
     higth = forms.IntegerField(required=None, min_value=0, max_value=1000,
-                                label='Высота (мм)', widget=form_wiget)
+                               label='Высота (мм)', widget=form_wiget)
     weigh = forms.ChoiceField(required=None,
-                                label='Плотность (г/м)', choices=paper_weigh)
+                              label='Плотность (г/м)', choices=paper_weigh)
     order = forms.IntegerField(required=None, min_value=0, max_value=100000,
-                                label='Тираж (шт)', widget=form_wiget)
-    dublicate = forms.BooleanField(required=None, widget=RadioSelect(choices=dublicate_choise))
+                               label='Тираж (шт)', widget=form_wiget)
+    dublicate = forms.BooleanField(required=None,
+                                   widget=widgets.RadioSelect(choices=dublicate_choise))
+
+
+material_choise = [('banner', 'Баннер'), ('sticker', 'Наклейка'), ('tabl', 'Табличка')]
+
+class SolventForm(forms.Form):
+    type_order = forms.CharField(initial='solvent', label='Широкоформатная печать',
+                                 widget=widgets.TextInput(attrs={'style': 'display: none'}))
+    width = forms.IntegerField(required=None, min_value=0, max_value=100,
+                               label='Ширина (м)', widget=form_wiget)
+    higth = forms.IntegerField(required=None, min_value=0, max_value=100,
+                               label='Высота (м)', widget=form_wiget)
+    weigh = forms.ChoiceField(required=None,
+                              label='Материал (г/м)', choices=material_choise)
--- order_cost/views.py
+++ order_cost/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
-from .forms import OffsetForm
+from .forms import OffsetForm, SolventForm
 
 # Create your views here.
 
 
 def offset_cost(request):
+    pass
 
+
+def result(data: dict):
+    return data
+
+
+def order_cost(request):
+    print('-----', request, '-----')
     if request.method == 'GET':
         form = OffsetForm(request.GET)
         if form.is_valid():
@@ -17,22 +25,6 @@ def offset_cost(request):
             context['form'] = form
             print(context)
     else:
+        context = {}
         context['form'] = OffsetForm()
-
     return render(request, template_name='order_cost_index.html', context=context)
-
-
-def result(data: dict):
-    print(data)
-    result = 0
-    for i in data.values():
-        print(i, result)
-        if isinstance(i, int):
-            result += i
-    if data['dublicate']:
-        result *= 1.5
-    return result
-
-
-def order_cost(request):
-    return offset_cost(request)
