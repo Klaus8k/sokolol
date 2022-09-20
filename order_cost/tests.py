2997aa04077071ea468c362177157cc250f966e2
Oleg Sokolov <klaus888@yandex.ru>
2022-09-20 18:08:11 +0300

    44

--- order_cost/tests.py
+++ order_cost/tests.py
@@ -1,26 +1,8 @@
 # Create your tests here.
+import requests
+import json
 
 
-def dec_x2(func):
-    def wrap(*args, **kwargs):
-        result = func(*args, **kwargs)
+with open('preference.json', 'r') as file:
+    print(json.loads(file.read()), sep='4')
 
-        return result
-    return wrap
-
-    
-# def dec_x2(x):
-#     return x * 2
-
-@dec_x2
-def summm(a,b):
-    return a+b
-
-
-# @dec_x2
-# def summm(a,b):
-#     return a+b
-
-if '__main__' == __name__:
-    print(summm(3,2))
-    # print(dec_x2(summm(3,2)))
