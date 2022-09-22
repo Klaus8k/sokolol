8e34159d2c99d7b3a0615c27f45fb422206c5a76
Oleg S home <tipkor@mail.ru>
2022-09-23 00:44:56 +0300

    json class with search

--- order_cost/tests.py
+++ order_cost/tests.py
@@ -1,25 +1,25 @@
 # Create your tests here.
-from asyncore import write
-from textwrap import indent
-import requests #type: ignore
+# import requests  # type: ignore
+from http import server
 import json
 import os
 
 print(os.getcwd())
 
 
-def read_json(): 
+def read_json():
     with open('order_cost/preference.json', 'r') as file:
         fileStr = file.read()
         jsonString = json.loads(fileStr)
     return jsonString
 
+
 def write_json():
-        with open('order_cost/preference.json', 'r') as file:
-            filestr = read_json()
-            for i in filestr.values():
-                if 'GlossDiv' in i.keys():
-                    print(i['GlossDiv'])
+    with open('order_cost/preference.json', 'r') as file:
+        filestr = read_json()
+        for i in filestr.values():
+            if 'GlossDiv' in i.keys():
+                print(i['GlossDiv'])
 
 # write_json()
 
@@ -30,12 +30,19 @@ class Json_obj():
     def __init__(self, file_json) -> None:
         self.file_json = file_json
         with open(self.file_json, 'r') as file:
-            self.j_str = json.load(file)
-
+            self.j_str = json.load(file)  # type: dict
 
     def read(self):
         return self.j_str
 
+    def search(self, search_key):
+
+        if search_key in self.j_str.keys():
+            return [search_key, self.j_str[search_key]]
+        else:
+            return 'Неверный ключ'
+
 
 a = Json_obj('order_cost/preference.json')
 print(a.read())
+print(a.search('banner'))
