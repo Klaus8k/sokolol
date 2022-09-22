d24797daac257b297e310d3101e13500730dc217
Oleg S home <tipkor@mail.ru>
2022-09-23 00:03:09 +0300

    json class

--- order_cost/tests.py
+++ order_cost/tests.py
@@ -1,4 +1,6 @@
 # Create your tests here.
+from asyncore import write
+from textwrap import indent
 import requests #type: ignore
 import json
 import os
@@ -6,7 +8,34 @@ import os
 print(os.getcwd())
 
 
-with open('order_cost/preference.json', 'r') as file:
-    jsonString = json.load(file)
-    
-    print(jsonString)
+def read_json(): 
+    with open('order_cost/preference.json', 'r') as file:
+        fileStr = file.read()
+        jsonString = json.loads(fileStr)
+    return jsonString
+
+def write_json():
+        with open('order_cost/preference.json', 'r') as file:
+            filestr = read_json()
+            for i in filestr.values():
+                if 'GlossDiv' in i.keys():
+                    print(i['GlossDiv'])
+
+# write_json()
+
+
+# JSON class work
+
+class Json_obj():
+    def __init__(self, file_json) -> None:
+        self.file_json = file_json
+        with open(self.file_json, 'r') as file:
+            self.j_str = json.load(file)
+
+
+    def read(self):
+        return self.j_str
+
+
+a = Json_obj('order_cost/preference.json')
+print(a.read())
--- order_cost/views.py
+++ order_cost/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.shortcuts import render #type: ignore
+from django.http import HttpResponseRedirect #type: ignore
 import json
 
 from .forms import OffsetForm, SolventForm, SolventSetForm
