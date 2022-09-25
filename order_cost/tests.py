818f2e23cc3bb8146758b46abe96993b1a54ce51
Oleg S home <tipkor@mail.ru>
2022-09-25 14:41:01 +0300

    test

--- order_cost/tests.py
+++ order_cost/tests.py
@@ -1,11 +1,21 @@
-import os
+import os, time
 import json
 
-print(os.getcwd())
-b = os.popen('cat 1.txt', 'r') # open file r or w
-print(b.read())
+# print(os.getcwd())
+# b = os.popen('cat 1.txt', 'r') # open file r or w
+# print(b.read())
 
-# os.system - run command
-a = [os.name, os.system('ls | grep 1'), os.environ['PATH']]
+# # os.system - run command
+# # os.environ - переменные командной оболочки
+# a = [os.name, os.system('ls | grep 1'), ]
+
+# c = os.path.split(os.getcwd())
+
+# print(c)
+
+
+a = time.gmtime()
 
 print(a)
+
+print()
