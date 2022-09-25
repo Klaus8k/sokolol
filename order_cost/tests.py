007849a43a6b9c101246839ca7b21c417551816a
Oleg S home <tipkor@mail.ru>
2022-09-25 13:53:36 +0300

    test os lib

--- order_cost/tests.py
+++ order_cost/tests.py
@@ -1,23 +1,11 @@
+import os
 import json
 
+print(os.getcwd())
+b = os.popen('cat 1.txt', 'r') # open file r or w
+print(b.read())
 
-class Json_obj():
-    def __init__(self, file_json) -> None:
-        self.file_json = file_json
-        
-    def read(self):
-        with open(self.file_json, 'r') as file:
-            cost = json.load(file)
-            return cost  # type: dict
+# os.system - run command
+a = [os.name, os.system('ls | grep 1'), os.environ['PATH']]
 
-    def write(self, data, order_type):
-        cost = self.read()
-        if order_type in cost.keys():
-            cost[order_type].update(data)
-        
-        with open(self.file_json, 'w') as file:
-            json.dump(cost, file, indent=4)
-
-            
-
-                
+print(a)
