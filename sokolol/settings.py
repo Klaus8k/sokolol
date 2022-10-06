19328fc8fb5db0ad5247916c4e69a005888145b7
Oleg S home <tipkor@mail.ru>
2022-10-06 22:16:44 +0300

    11

--- sokolol/settings.py
+++ sokolol/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SKsokolol']
-SECRET_KEY = 'django-insecure-i@0ss2$h7qk)dd=8meht--w$l*tfnyc0=ovz9jve-*jyuj-45+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
