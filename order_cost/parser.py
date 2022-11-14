ff454e82c3ce41c12f33b9d64f0df54a03e5a479
Oleg Sokolov <klaus888@yandex.ru>
2022-11-14 18:14:16 +0300

    selenium

--- order_cost/parser.py
+++ order_cost/parser.py
@@ -1,7 +1,7 @@
 import os
 from selenium import webdriver
 
-os.environ['PATH'] += r"C:/Selenium_drivers"
-print(os.environ.get('PATH'), os.getcwd())
+os.environ['PATH'] += r";C:\Selenium_drivers"
+print('-----------------',os.environ.get('PATH'), '-----------------')
 
 driver = webdriver.Chrome()
