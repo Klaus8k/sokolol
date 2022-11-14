3cf07f4b499843fc1079e847313732e847b2e42d
Oleg S home <tipkor@mail.ru>
2022-11-14 23:47:00 +0300

    ууу

--- order_cost/parser.py
+++ order_cost/parser.py
@@ -1,9 +1,12 @@
 import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 
 os.environ['PATH'] += r";C:/Selenium_drivers"
 
 driver = webdriver.Chrome()
 driver.get(r'https://demo.seleniumeasy.com/jquery-download-progress-bar-demo.html')
+WebDriverWait(driver, timeout = 5)
 btn = driver.find_element(By.ID, 'downloadButton')
+btn.click()
