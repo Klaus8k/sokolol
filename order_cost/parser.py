a62542dead2a9b4edd44a31d2178d9d8dfaa785f
Oleg S home <tipkor@mail.ru>
2022-11-15 00:51:33 +0300

    gmprint parce selenium

--- order_cost/parser.py
+++ order_cost/parser.py
@@ -2,11 +2,22 @@ import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.chrome.options import Options
 
+
+options = Options()
+options.add_argument('--ignore-certificate-errors')
 os.environ['PATH'] += r";C:/Selenium_drivers"
 
-driver = webdriver.Chrome()
-driver.get(r'https://demo.seleniumeasy.com/jquery-download-progress-bar-demo.html')
-WebDriverWait(driver, timeout = 5)
-btn = driver.find_element(By.ID, 'downloadButton')
+driver = webdriver.Chrome(options=options)
+driver.get(r'https://gmprint.ru/')
+driver.implicitly_wait(3)
+btn = driver.find_element(By.ID, 'its_my_city')
 btn.click()
+nextt = driver.find_element(By.LINK_TEXT, 'Визитки')
+nextt.click()
+print(driver.current_url)
+
+
+
+
