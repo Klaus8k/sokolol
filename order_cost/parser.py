d948eb0683afe4970c80d00b57c9c78e873bfe42
Oleg Sokolov <klaus888@yandex.ru>
2022-11-15 18:35:45 +0300

    selen def

--- order_cost/parser.py
+++ order_cost/parser.py
@@ -4,19 +4,24 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 
+os.environ['PATH'] += r";C:/Selenium_drivers"
+target_url = r'https://gmprint.ru/'
 
 options = Options()
 options.add_argument('--ignore-certificate-errors')
-os.environ['PATH'] += r";C:/Selenium_drivers"
-
 driver = webdriver.Chrome(options=options)
-driver.get(r'https://gmprint.ru/')
-driver.implicitly_wait(3)
-btn = driver.find_element(By.ID, 'its_my_city')
-btn.click()
-nextt = driver.find_element(By.LINK_TEXT, 'Визитки')
-nextt.click()
-print(driver.current_url)
+driver.get(target_url)
+
+def press_btn(driver, id : str):
+    driver.implicitly_wait(3)
+    btn = driver.find_element(By.ID, id)
+    btn.click()
+
+
+if __name__ == '__main__':
+    start = 'its_my_city'
+
+    press_btn(driver=driver, id=start)
 
 
 
