f56efc3aab9e02ee8c9e0278558b8611b77ef7fd
Oleg Sokolov <klaus888@yandex.ru>
2022-11-30 11:21:58 +0300

    1

--- order_cost/parsers/parser.py
+++ order_cost/parsers/parser.py
@@ -30,7 +30,8 @@ class Parse_unit(webdriver.Firefox):
         else:
             os.environ['PATH'] += r";C:/Selenium_drivers"
             options = Options()
-            # options.headless = True
+            options.page_load_strategy = 'normal'
+            options.headless = True
             options.binary_location = r'C:/Program Files/Mozilla Firefox/firefox.exe'
             super(Parse_unit, self).__init__(options=options)
 
@@ -53,43 +54,38 @@ if __name__ == '__main__':
         with Parse_unit() as m_grup:
             m_grup.land_first_page()
             m_grup.find_element(By.ID, 'its_my_city').click()
- 
-# TODO js hack method, bug. Must be search other way
 
+# TODO js hack method, bug. Must be search other way
             btn_leaflets = m_grup.find_element(By.CLASS_NAME, 'menu_leaflets')
             print(btn_leaflets.is_enabled())
             m_grup.execute_script("arguments[0].click();", btn_leaflets)
-            # btn_leaflets.click()
 
-            # element = WebDriverWait(m_grup, 20).until(EC.visibility_of_element_located((By.CLASS_NAME, 'menu_leaflets')))
-            # m_grup.execute_script("arguments[0].click();", element)
-
-            
-
-            m_grup.find_element(By.CSS_SELECTOR, 'span[id^="select2-density"]').click()
-            destiny = m_grup.find_element(By.CLASS_NAME, 'select2-results__options')
-            destiny_list = destiny.find_elements(By.CSS_SELECTOR, 'li[role="option"]')
+            m_grup.find_element(
+                By.CSS_SELECTOR, 'span[id^="select2-density"]').click()
+            destiny = m_grup.find_element(
+                By.CLASS_NAME, 'select2-results__options')
+            destiny_list = destiny.find_elements(
+                By.CSS_SELECTOR, 'li[role="option"]')
 
             x = None
             for i in destiny_list:
                 if i.text == '170 г/м² бумага глянц.':
                     x = i
                     break
-            
+
             x.click()
-            
 
             other_format = m_grup.find_element(By.ID, 'other_format_button')
             other_format.click()
-            m_grup.find_element(By.CSS_SELECTOR, 'input[name="formatX"]').send_keys(100)
-            m_grup.find_element(By.CSS_SELECTOR, 'input[name="formatY"]').send_keys(100)
-            m_grup.implicitly_wait(10)
-# TODO УСЛОВНОЕ ОЖИДАНИЕ ДЛЯ РЕЗУЛЬТАТА, ВОЗМОЖНА ПРОКРУТКА НУЖНА
-            result = WebDriverWait(m_grup, 20).until(
-                EC.text_to_be_present_in_element((
-                By.CLASS_NAME, 'b-price__text')))
+            m_grup.find_element(
+                By.CSS_SELECTOR, 'input[name="formatX"]').send_keys(200)
+            m_grup.find_element(
+                By.CSS_SELECTOR, 'input[name="formatY"]').send_keys(200)
 
-            print(result)
-                    
+# TODO УСЛОВНОЕ ОЖИДАНИЕ ДЛЯ РЕЗУЛЬТАТА, ВОЗМОЖНА ПРОКРУТКА НУЖНА
+            WebDriverWait(m_grup, 20).until(EC.visibility_of_element_located(
+                (By.CSS_SELECTOR, 'span[class="b-price__text"')))
+
+            print('result ------->', m_grup.find_element(By.CSS_SELECTOR, 'span[class="b-price__text"').text)
 
     parce_m_grup()
