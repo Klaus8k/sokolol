4c4391b123d1efbdd57caf54ef2feb6d3bf92d1d
Klaus8k <klaus888@yandex.ru>
2023-01-10 23:57:53 +0200

    1

--- order_cost/models.py
+++ order_cost/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Solvent(models.Model):
+    date = models.TimeField(verbose_name='date', auto_now_add=True)
+    type = models.CharField('type', max_length=20)
+    cost = models.IntegerField('cost')
+
