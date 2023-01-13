63da6e69a20a9214c3fa1f2ef99a550d55c687fb
Oleg Sokolov <klaus888@yandex.ru>
2023-01-13 18:11:50 +0300

    add date

--- order_cost/models.py
+++ order_cost/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 class Solvent(models.Model):
-    date = models.TimeField(verbose_name='date', auto_now_add=True)
+    date = models.DateField(verbose_name='date', auto_now_add=True)
     type = models.CharField('type', max_length=20)
     cost = models.IntegerField('cost')
 
