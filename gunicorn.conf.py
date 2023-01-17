f7e6635686caa0c3215122c3ec4103d5eba255cb
Oleg Sokolov <klaus888@yandex.ru>
2023-01-17 12:43:39 +0300

    1

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -147,7 +147,7 @@ tmp_upload_dir = None
 #
 
 errorlog = 'logs/gunicorn_log.txt'
-loglevel = 'info'
+loglevel = 'warning'
 accesslog = 'logs/gunicorn_log.txt'
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 
