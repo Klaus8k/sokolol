4302b33ecd040e09a26f763c5faf0322091186f2
Oleg Sokolov <klaus888@yandex.ru>
2023-02-18 13:37:08 +0300

    add foreignkey

--- order_cost/admin.py
+++ order_cost/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Offset_model, Solvent_model
+from .models import Offset_model, Solvent_model, Type_Order
 
 # Register your models here.
 
 admin.site.register(Solvent_model)
 admin.site.register(Offset_model)
+admin.site.register(Type_Order)
