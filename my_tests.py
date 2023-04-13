53cc4f90fbaf2bdf7ac2f8806206d20394e0efe5
Oleg <klaus888@yandex.ru>
2023-04-14 00:25:25 +0300

    leet test

--- my_tests.py
+++ my_tests.py
@@ -1,59 +1,15 @@
-# # ------ Пример работы с sql-lite
-# import sqlite3
-
-# con = sqlite3.connect('db.sqlite3')
-# cur = con.cursor()
-
-# # data = [(33,23),(44,43),(44,43)]
-# # cur.executemany('insert into test_table values(?,?)', data)
-# # con.commit()
-
-# def show_test_table():
-#     print(cur.execute('select * from test_table').fetchall())
+def romanToInt(s: str) -> int:
+    roman_letter = ('I', 'V', 'X', 'L', 'C', 'D', 'M')
+    sum = 0
+    mid_sum = s[0]
+    for i in s:
+        index = s.index(i)
+        if  index < len(s) and index < s.index(i+1):
+            mid_sum += 1
 
 
-# show_test_table()
 
 
-# import getpass
-# print(getpass.getuser())
 
 
-# a = '5'
-# b = '10'
-
-
-# c = '1'
-
-# print(id(a), id(b), id(c))
-# b = b[:1]
-
-# a = str(int(b) +4)
-
-# print(a, b, c)
-
-# print(id(a), id(b), id(c))
-
-# https://medium.com/@kirushenski/visual-studio-code-настройка-и-применение-часть-1-7f1a26806522
-
-
-a = 'Аргентина анит негра'
-
-
-class Polin():
-
-    def __init__(self, string: str) -> None:
-        self.string = ''.join(string.split()).lower()
-        half = len(self.string) // 2
-        if half == 0:
-            return 0
-        self.half_str = self.string[:half]
-        self.half_rev = self.half_str[::-1]
-
-    def polin(self):
-        print(self.string[len(self.string) // 2:])
-        return self.half_rev == self.string[len(self.string) // 2:]
-
-
-x = Polin(a)
-print(x.polin())
+assert romanToInt('III') != 3, 'not true'
