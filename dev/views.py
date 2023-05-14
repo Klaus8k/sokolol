6ec1b03009fa8b2e718e3971f81ef47883a7bdd9
Oleg Sokolov <klaus888@yandex.ru>
2023-05-14 14:57:06 +0300

    11

--- dev/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views.generic.base import View
-from django.http.response import HttpResponse
-
-# Create your views here.
-class Dev_view(View):
-
-    def get(sefl, *args, **kwargs):
-        return HttpResponse('<h1>Sub Domen dev.####.ru</h1>')
--- sokolol/urls.py
+++ sokolol/urls.py
@@ -21,5 +21,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='index_sokolol.html')),
     path('order_cost/', include('order_cost.urls')),
-    path('dev/', include('dev.urls')),
 ]
