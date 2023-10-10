d82a8c0fbf126487c044ba2a4ec810b5444baad2
Oleg Sokolov <klaus888@yandex.ru>
2023-10-10 17:23:37 +0300

    timer add

--- sokolol/urls.py
+++ sokolol/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='index_sokolol.html')),
     path('order_cost/', include('order_cost.urls')),
-    path('tipkor/', TemplateView.as_view(template_name='tipkor.html'))
+    path('tipkor/', TemplateView.as_view(template_name='tipkor.html')),
+    path('timer/', TemplateView.as_view(template_name='timer.html'))
 ]
